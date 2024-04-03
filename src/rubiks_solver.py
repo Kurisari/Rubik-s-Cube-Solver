33452551cbb737d6b9142a8163bdb93f25962850
Cristian Aragón <crisarag8.sal@gmail.com>
2024-04-02 18:48:05 -0600

    Implementación de bfs breadth

--- src/rubiks_solver.py
+++ src/rubiks_solver.py
@@ -1,5 +1,7 @@
 import os
 import sys
+from collections import deque
+import copy
 script_dir = os.getcwd()
 func_dir = os.path.join(script_dir)
 sys.path.append(func_dir)
@@ -8,6 +10,13 @@ from lib import rubiks_cube as rc
 class RubiksSolver:
     def __init__(self):
         self.cube = rc.RubiksCube()
+    
+    def is_solved(self, cube):
+        for i in range(0, 45, 9):
+            face_color = cube.stickers[i]
+            if any(sticker != face_color for sticker in cube.stickers[i:i+9]):
+                return False
+        return True
 
     def shuffle_cube(self, n=None, movement_list=None):
         if movement_list is None:
@@ -15,8 +24,32 @@ class RubiksSolver:
         else:
             self.cube.list_shuffle(movement_list)
     
-    def breadth_first_search(self):
-        pass
+    def breadth_first_search(self): # Máximo 5 movimientos aleatorios
+        queue = deque([(self.cube, [])])
+        visited = set()
+        while queue:
+            current_cube, moves = queue.popleft()
+            if self.is_solved(current_cube):
+                return moves
+            for move in [
+                ["x", "U", "L"],
+                ["x", "U", "R"],
+                ["x", "D", "L"],
+                ["x", "D", "R"], 
+                ["y", "U", "L"],
+                ["y", "U", "R"],
+                ["y", "D", "L"],
+                ["y", "D", "R"],
+                ["z", "L", "U"],
+                ["z", "L", "D"],
+                ["z", "R", "U"],
+                ["z", "R", "D"]]:
+                new_cube = copy.deepcopy(current_cube)
+                new_cube.move(move)
+                
+                if new_cube not in visited:
+                    queue.append((new_cube, moves + [move]))
+                    visited.add(new_cube)
     
     def best_first_search(self):
         pass
@@ -27,8 +60,10 @@ class RubiksSolver:
     def print_cube(self):
         self.cube.print_cube()
 
-lista = [["x", "U", "L"], ["y", "D", "L"], ["z", "L", "D"]]
+# lista = [["x", "U", "L"], ["y", "D", "L"], ["z", "L", "D"]]
+lista = [["x", "D", "R"], ["x", "U", "L"], ["y", "U", "R"]]
 prueba = RubiksSolver()
 prueba.print_cube()
-prueba.shuffle_cube(movement_list=lista)
+prueba.shuffle_cube(5)
 prueba.print_cube()
+print(prueba.breadth_first_search())
